bd4e558d5be2ceba0a23e25e236299f80a24c4e7
Het Patel <hetpatel2220087607@gmail.com>
2025-08-23 07:51:02 +0000

    in the signup section make sure that i can signup with google

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,7 +2,7 @@
 'use client';
 
 import {initializeApp, getApp, getApps} from 'firebase/app';
-import {getAuth} from 'firebase/auth';
+import {getAuth, GoogleAuthProvider} from 'firebase/auth';
 import {getFirestore} from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -19,5 +19,6 @@ const firebaseConfig = {
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const db = getFirestore(app);
+const googleProvider = new GoogleAuthProvider();
 
-export {app, auth, db};
+export {app, auth, db, googleProvider};
