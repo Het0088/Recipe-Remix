3350cd967aab9b5e769a5a1f83c70dbde3c15352
Het Patel <hetpatel2220087607@gmail.com>
2025-09-08 16:14:56 +0000

    https://images.unsplash.com/photo-1498579809087-ef1e558fd1da?q=80&w=2070

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
       <body
         className={cn('min-h-screen font-body antialiased')}
         style={{
-          backgroundImage: `url(https://images.unsplash.com/photo-1494390248081-4e521a5940db?q=80&w=1106&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)`,
+          backgroundImage: `url(https://images.unsplash.com/photo-1498579809087-ef1e558fd1da?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)`,
           backgroundSize: 'cover',
           backgroundPosition: 'center',
           backgroundAttachment: 'fixed',
