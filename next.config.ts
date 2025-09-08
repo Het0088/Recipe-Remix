332f3287b70c53f174fe6f06a75d4c129199c996
Het Patel <hetpatel2220087607@gmail.com>
2025-09-08 13:04:29 +0000

    And idk why but the reaction time of the next app is too slow, do someth

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,12 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+      maxDuration: 60,
+    },
+  },
 };
 
 export default nextConfig;
